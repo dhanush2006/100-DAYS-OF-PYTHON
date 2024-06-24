0e41e06b5833baa980270b7de4f60117e30a117c
DHANUSHPRAKASH V C <75581083+dhanush2006@users.noreply.github.com>
2024-06-24 22:45:12 +0530

    Create Day -1.py

---
+++ Day -1.py
@@ -0,0 +1,61 @@
+#Lesson - 1
+#print("Hello World!")
+
+#Exercise - 1
+#print("Day 1 - Python Print Function")
+#print("The function is declared like this:")
+#print("print('what to print')")
+#print('print("what to print")')
+
+#Lesson - 2
+#print("Hello \nworld")
+#print("Hello" +" "+"Dhanush")
+
+#Exercise - 2
+#print("Day 1 - String Manipulation")
+#print("String Concadination is done with the "+'"+"'+" sign")
+#print('e.g. print("Hello " + "world")')
+#print("New lines can be created with a blackslash and n.")
+
+#Lesson - 3
+#input("What is my name?")
+#print("What is my name?")
+#print("Hello "+ input("What is your name?"))
+
+#Exercise - 3
+#print (len (input("What is your name?")))
+
+#Lesson - 4
+#name = input("What is yaur name?")
+#print(name)
+
+#name = "dhanush"
+#print(name)
+#name = "prakash"
+#print(name)
+
+#name = input("What is your name?")
+#length = len(name)
+#print(length)
+#print (name)
+
+#Exercise - 4
+#a = input("a:")
+#b = input("b:")
+#a, b = b,a
+#temp = a
+#a = b
+#b = temp
+#print("a = " + a)
+#print("b = " + b)
+
+#Lesson - 5 (naming of variables)
+
+#user_name = input("What is your name?")
+
+#Final Project
+#print("Welcome to the band name generator!\n")
+#user_city = input("What is the city you live\n")
+#user_pet = input("What is your favourite pet\n")
+#band_name = user_city + user_pet
+#print("Your Band Name is "+ band_name)
