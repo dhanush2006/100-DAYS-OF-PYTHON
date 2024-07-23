e63a9cfa1c61754f3314a2bfa2a6e505904e0745
DHANUSHPRAKASH V C <75581083+dhanush2006@users.noreply.github.com>
2024-07-23 23:35:57 +0530

    Add files via upload

---
+++ P - 3.py
@@ -0,0 +1,4 @@
+students_height = input("Input a list of students heights").split()
+for n in range (0, len(students_height)):
+    students_height[n] = int(students_height[n])
+print(students_height)
