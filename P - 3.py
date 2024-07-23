d9d3edf1a1bf79c5c714cf03bdf41a66d5728078
DHANUSHPRAKASH V C <75581083+dhanush2006@users.noreply.github.com>
2024-07-23 23:36:42 +0530

    Delete P - 3.py

--- P - 3.py
+++
@@ -1,4 +0,0 @@
-students_height = input("Input a list of students heights").split()
-for n in range (0, len(students_height)):
-    students_height[n] = int(students_height[n])
-print(students_height)
